d4737e12d832cdd93af30123483e72d6a44b0828
Harunari Takata <tkt0430@keio.jp>
2025-09-25 12:42:56 +0900

    add sendRPC method for managing RPC client calls and error handling

--- rpc.go
+++ rpc.go
@@ -1,7 +1,8 @@
 package main
 
+import "fmt"
+
 const (
-	// RPC names
 	ElectionRPC    = "Node.Election"
 	CoordinatorRPC = "Node.Coordinator"
 	PingRPC        = "Node.Ping"
@@ -16,5 +17,20 @@ func (n *Node) Coordinator(args *struct{}, reply *struct{}) error {
 }
 
 func (n *Node) Ping(args *struct{}, reply *struct{}) error {
+	return nil
+}
+
+func (n *Node) sendRPC(targetAddr string, method string, args interface{}, reply interface{}) error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	client, ok := n.rpcClients[targetAddr]
+	if !ok {
+		return fmt.Errorf("no RPC client for address: %s", targetAddr)
+	}
+	if err := client.Call(method, args, reply); err != nil {
+		n.rpcClients[targetAddr].Close()
+		delete(n.rpcClients, targetAddr)
+		return err
+	}
 	return nil
 }
