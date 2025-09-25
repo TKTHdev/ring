0249dc7be4dfe069a2236ac6a8c10cf39b3a3dde
Harunari Takata <tkt0430@keio.jp>
2025-09-25 12:33:34 +0900

    RPC Args/Reply

---
+++ rpc_structs.go
@@ -0,0 +1,13 @@
+package main
+
+type ElectionArgs struct{}
+
+type ElectionReply struct{}
+
+type CoordinatorArgs struct{}
+
+type CoordinatorReply struct{}
+
+type PingArgs struct{}
+
+type PingReply struct{}
